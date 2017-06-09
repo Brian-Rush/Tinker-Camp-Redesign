fb98d57a0f240d6160c98961b50f1e08e02db76b
YcleptInsan <nickmwise@gmail.com>
2017-06-09 13:49:10 -0700

    Add Client form, delete all client and stylist route, edit stylist route, Update route.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -25,6 +25,55 @@ namespace Salon
       Get["/stylists/new"] = _ => {
         return View["stylists_form.cshtml"];
       };
+      Post["stylists/new"] = _ => {
+        Stylist newStylist = new Stylist(Request.Form["stylist-name"]);
+        newStylist.Save();
+        return View["success.cshtml"];
+      };
+      Get["/clients/new"] = _ => {
+        List<Client> AllClient = Client.GetAll();
+        return View["clients_form.cshtml", AllClient];
+      };
+      Post["/clients/new"] = _ => {
+        Client newClient = new Client(Request.Form["client-name"], Request.Form["stylist-id"]);
+        newClient.Save();
+        return View["success.cshtml"];
+       };
+       Post["/clients/delete"] = _ => {
+         Client.DeleteAll();
+         return View["cleared.cshtml"];
+       };
+       Get["/stylists/{id}"] = parameters => {
+         Dictionary<string, object> model = new Dictionary<string, object>();
+         var SelectedStylist = Stylist.Find(parameters.id);
+         var StylistClients = SelectedStylist.GetClients();
+         model.Add("stylist", SelectedStylist);
+         model.Add("clients", StylistClients);
+
+         return View["stylist.cshtml", model];
+       };
+       Get["/stylist/edit/{id}"] = parameters => {
+         Dictionary<string, object> model = new Dictionary<string, object>();
+         Stylist SelectedStylist = Stylist.Find(parameters.id);
+         List<Client> StylistClients = SelectedStylist.GetClients();
+         model.Add("stylist", SelectedStylist);
+         model.Add("restaurants", StylistClients);
+         return View["stylist_edit.cshtml", model];
+       };
+       Patch["/stylist/edit/{id}"] = parameters => {
+         Stylist SelectedStylist = Stylist.Find(parameters.id);
+         SelectedStylist.Update(Request.Form["stylist-name"]);
+
+         return View["success.cshtml"];
+       };
+       Get["/stylist/delete/{id}"] = parameters => {
+         Dictionary<string, object> model = new Dictionary<string, object>();
+         Stylist SelectedStylist = Stylist.Find(parameters.id);
+         List<Client> StylistClients = SelectedStylist.GetClients();
+         model.Add("stylist", SelectedStylist);
+         model.Add("restaurants", StylistClients);
+         return View["stylist_delete.cshtml", model];
+       };
     }
   }
 }
