2253dad63a0a817794d22afb015069751fd54cae
Hunter Parks and Nick Wise <hunter+nick@>
2017-06-19 11:05:38 -0700

    add Scripts and also add Accounts object file

---
+++ Objects/Account.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Account.Objects
+{
+  public class Account
+  {
+    private string _firstName;
+    private string _lastName;
+    private string _address;
+    private string _city;
+    private string _state;
+    private string _zip;
+    private string _phone;
+    private string _email;
+    private string _code;
+
+    public Account(string FirstName, string LastName, string Address, string City, string State, string Zip, string Phone, string email, string code)
+    {
+      _firstName = FirstName;
+      _lastName = LastName;
+      _address = Address;
+      _city = City;
+      _state = State;
+      _zip = Zip;
+      _phone = Phone;
+      _email = Email;
+      _code = Code;
+    }
+
+    private string GetFirstName()
+    {
+      return _firstName;
+    }
+    private string GetSecondName()
+    {
+      return _secondName;
+    }
+    private string GetAddress()
+    {
+      return _address;
+    }
+    private string GetCity()
+    {
+      return _city;
+    }
+    private string GetState()
+    {
+      return _state;
+    }
+    private string GetZip()
+    {
+      return _zip;
+    } 
+    private string GetPhone()
+    {
+      return _phone;
+    }
+    private string GetCode()
+    {
+      return _code;
+    }
+  }
+}
